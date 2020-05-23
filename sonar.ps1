fcd773da8752626c90a86b20f6d45c4253b006a7
Ruben Lelieveld <ruben@rubium.nl>
2020-05-23 13:06:35 +0200

    Removed incorrect files

--- sonar.ps1
+++
@@ -1,5 +0,0 @@
-dotnet test .\DddEfteling.Tests\DddEfteling.Tests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
-dotnet build-server shutdown
-dotnet sonarscanner /k:"EF" /d:sonar.host.url="http://192.168.1.246:9000" /d:sonar.coverage.exclusions="**Test*.cs,**/Program.cs,**/Startup.cs"  /d:sonar.cs.opencover.reportsPaths=DddEfteling.Tests/coverage.opencover.xml
-dotnet build .
-dotnet sonarscanner end
