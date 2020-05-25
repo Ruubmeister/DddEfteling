5e3c51ad28f5852a2f9ae0934f41edbc98de63ba
Ruben Lelieveld <ruben@rubium.nl>
2020-05-25 19:17:17 +0200

    Added visitors and splitted park bit better

---
+++ DddEfteling.Tests/Visitors/Entities/VisitorTest.cs
@@ -0,0 +1,23 @@
+﻿using DddEfteling.Visitors.Visitors.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DddEfteling.Tests.Visitors.Entities
+{
+    public class VisitorTest
+    {
+        [Fact]
+        public void Construct_createVisitor_expectVisitor()
+        {
+            DateTime dateOfBirth = DateTime.Now;
+            dateOfBirth.Subtract(TimeSpan.FromDays(365*20));
+            Visitor visitor = new Visitor(dateOfBirth, 1.73);
+
+            Assert.Equal(dateOfBirth, visitor.DateOfBirth );
+            Assert.False(visitor.Guid == Guid.Empty);
+            Assert.Equal(1.73, visitor.Length);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ddd-efteling
-Demo repo to experiment with domain driven development
+Repo to learn domain driven development. **Note that this has no relationship with Efteling itself, this is a project where the theme park is used as the domain to work with.**
+
+## Introduction
+
+Using the book "Implementing Domain-Driven Design", I wanted to have a subject to work with. That is this repo! So feel free to look around, or fork or anything if you'd like.
+
+24 May 2019: Still working on setting up the base functionality.
