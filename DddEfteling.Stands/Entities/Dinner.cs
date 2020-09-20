8a757538f64b43336b1f0fcebe4ffc46bcf10823
Ruben Lelieveld <rlelieveld@ilionx.com>
2020-09-20 14:26:42 +0200

    Implemented dinners and DinnerBuilder

---
+++ DddEfteling.Stands/Entities/Dinner.cs
@@ -0,0 +1,29 @@
+﻿using DddEfteling.Stands.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DddEfteling.Shared.Entities
+{
+    public class Dinner
+    {
+        public Dinner() { }
+        public Dinner(HashSet<Product> meals, HashSet<Product> drinks) { 
+            if(meals.Where(item => !item.Type.Equals(ProductType.Meal)).Any())
+            {
+                throw new ArgumentException("Given meals contain other types");
+            }
+            if (drinks.Where(item => !item.Type.Equals(ProductType.Drink)).Any())
+            {
+                throw new ArgumentException("Given meals contain other types");
+            }
+            this.Meals = meals;
+            this.Drinks = drinks;
+        }
+
+        public HashSet<Product> Meals { get; }
+
+        public HashSet<Product> Drinks { get; }
+    }
+}
